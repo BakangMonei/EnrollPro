3517636124b86ea44f54dade60fe4806807297d1
BakangMonei <bakangmonei2@gmail.com>
2024-03-07 00:52:54 +0200

    Designing the backend

--- app/src/main/java/com/assignment/enrollpro/Screens/LectureActivity.java
+++ app/src/main/java/com/assignment/enrollpro/Screens/LectureActivity.java
@@ -3,10 +3,15 @@ package com.assignment.enrollpro.Screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.assignment.enrollpro.R;
 
 public class LectureActivity extends AppCompatActivity {
+
+    private ImageView itemsPurchasedImageView, itemsUpdateImageView, itemsDeleteImageView, itemsViewImageView;
+    private TextView itemsPurchasedTextView, itemsUpdateTextView, itemsDeleteTextView, itemsViewTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
