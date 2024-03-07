869b0f94fdf2a2996f9994d1e7add5588982ac1e
BakangMonei <bakangmonei2@gmail.com>
2024-03-07 03:26:02 +0200

    Fixing the UI

---
+++ app/src/main/java/com/assignment/enrollpro/Activities/DeletedBookingExamActivity.java
@@ -0,0 +1,18 @@
+package com.assignment.enrollpro.Activities;
+// This is where we view 'Deleted Records' and be able to restore them
+
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.assignment.enrollpro.R;
+
+
+public class DeletedBookingExamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deleted_booking_exam);
+    }
+}
---
+++ app/src/main/java/com/assignment/enrollpro/Activities/ViewBookingUpdateExamActivity.java
@@ -0,0 +1,20 @@
+package com.assignment.enrollpro.Activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.assignment.enrollpro.R;
+
+public class ViewBookingUpdateExamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_booking_update_exam);
+    }
+}
